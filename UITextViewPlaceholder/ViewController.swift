4840360cac451dfb09473a3d16eae55ad75927d2
Lawrence F MacFadyen <larry.macfadyen@gmail.com>
2016-03-20 13:42:34 -0400

    Initial Commit

---
+++ UITextViewPlaceholder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITextViewPlaceholder
+//
+//  Created by Lawrence F MacFadyen on 2016-03-20.
+//  Copyright © 2016 LawrenceM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
